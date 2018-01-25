a46a6887b45aa2e17390973d2655712a245909d8
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-25 20:57:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,16 @@
 class Student #parent class /"super"
 
+  attr_accessor :chatty_student
+
   def hello
-    @greeting = true
-  end
-  #   "Hey there! I'm so excited to learn stuff."
+  #   @greeting = true
   # end
+    "Hey there! I'm so excited to learn stuff."
+  end
 
   def raise_hand
-    @hand_raised = true
-    # "Pick me!"
+    # @hand_raised = true
+    "Pick me!"
   end
 
 end
